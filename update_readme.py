136d96d108c6d1c72f80fceeadca23df7f21dd8f
Juanfu224 <juanfu224@gmai.com>
2025-09-30 20:38:05 +0200

    Cambios en update_readme.py para guardar historico de pruebas en README.md

--- update_readme.py
+++ update_readme.py
@@ -1,25 +1,30 @@
 import subprocess
+from datetime import datetime
+
 
 def run_tests():
+    dia_hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M")
     try:
         subprocess.check_call(["pytest", "-q"])
-        return "✅ Tests correctos"
+        return f"✅ {dia_hora_actual} Tests correctos"
     except subprocess.CalledProcessError:
-        return "❌ Tests fallidos"
+        return f"❌ {dia_hora_actual} Tests fallidos"
+
 
 def update_readme(status: str):
+    new_lines = []
+
     with open("README.md", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
-    new_lines = []
     for line in lines:
         new_lines.append(line)
         if line.strip() == "## Estado de los tests":
             new_lines.append(status + "\n")
-            break
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.writelines(new_lines)
+
 
 if __name__ == "__main__":
     status = run_tests()
