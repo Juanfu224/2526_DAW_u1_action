0e2634b6b85f44a6cb34ded4d920001af7aacd84
Juanfu224 <88267143+Juanfu224@users.noreply.github.com>
2025-10-09 08:47:18 +0000

    Deploying to gh-pages from @ Juanfu224/2526_DAW_u1_action@4d83ee3d0ad7b4059f99352077fa6dd8ddb16fed 🚀

---
+++ html/search/all_11.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['read_0',['read',['../structcmd_8h.html#a9c7b76d5266903891c803132d51ccb90',1,'structcmd.h']]],
+  ['readme_5fmejora_1',['README_mejora',['../md_README_mejora.html',1,'']]],
+  ['restypedef_2ecpp_2',['restypedef.cpp',['../restypedef_8cpp.html',1,'']]]
+];
---
+++ html/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme_5fmejora_0',['README_mejora',['../md_README_mejora.html',1,'']]]
+];
